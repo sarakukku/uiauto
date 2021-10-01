01dcc2af77b03556f8f96dfa49ebbb7cc797f3fe
Kukku Sara <sarakukku@gmail.com>
2021-09-30 16:34:56 -0800

    Added cypress UI auto project

---
+++ CypressAutomation/cypress/integration/pageObjects/homePage.js
@@ -0,0 +1,19 @@
+class HomePage {
+  getEditBox() {
+    return cy.get(":nth-child(1) > .form-control");
+  }
+
+  getTwoWayDataBinding() {
+    return cy.get(":nth-child(4) > .ng-untouched");
+  }
+
+  getEntrepreneur() {
+    return cy.get("#inlineRadio3");
+  }
+
+  getShopTab() {
+    return cy.get(":nth-child(2) > .nav-link");
+  }
+}
+
+export default HomePage;
